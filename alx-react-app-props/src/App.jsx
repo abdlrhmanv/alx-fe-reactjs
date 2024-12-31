4c5a129c48a130a0534da8524acf9dd3fe27d4ff
Abdlrhman <abdlrhmanv@icloud.com>
2024-12-31 21:14:59 +0200

    Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider>
+    <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
   );
