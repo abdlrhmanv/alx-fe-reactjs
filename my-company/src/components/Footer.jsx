c429dd5863d2e5f857f4c7fe6b934571d53181d7
Abdlrhman <abdlrhmanv@icloud.com>
2024-12-31 21:44:37 +0200

    Build a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    const footerStyle = {
+      textAlign: 'center',
+      padding: '10px',
+      backgroundColor: '#333',
+      color: 'white',
+      marginTop: '20px',
+    };
+  
+    return <footer style={footerStyle}>© 2024 My Company. All rights reserved.</footer>;
+  }
+  
+  export default Footer;
