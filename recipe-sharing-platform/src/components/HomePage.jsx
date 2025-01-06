51c4e49ebfd93451655901b3c21ccd1fe42b5aba
Abdlrhman <abdlrhmanv@icloud.com>
2025-01-06 07:39:36 +0200

    Building the Recipe Detail Page

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
     const [recipes, setRecipes] = useState([]);
@@ -16,10 +17,11 @@ function HomePage() {
             <h1 className="text-2xl font-bold text-center mb-6">Recipe Collection</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
              {recipes.map((recipe) => (
-                <div
-                    key={recipe.id}
-                    className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg hover:scale-105 transition-transform duration-300"
-                >
+              <Link
+              to={`/recipe/${recipe.id}`}
+              key={recipe.id}
+              className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg hover:scale-105 transition-transform duration-300"
+              >
                 <img
                   src={recipe.image}
                   alt={recipe.title}
@@ -29,7 +31,7 @@ function HomePage() {
                   <h2 className="text-lg font-semibold mb-2">{recipe.title}</h2>
                   <p className="text-gray-600">{recipe.summary}</p>
                 </div>
-                </div>
+                </Link>
               ))}
         </div>
     </div>
