21e1c3e85569d9b6f0a9671b94ca3f1d58aa76c8
Abdlrhman <abdlrhmanv@icloud.com>
2025-01-06 23:47:46 +0200

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
+const BASE_URL = "https://api.github.com/search/users?q";
 
 export async function fetchAdvancedUserData({ username = "", location = "", minRepos = 0 }) {
     try {
